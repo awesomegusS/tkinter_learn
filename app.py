739043e718dca593b1bc47510cc2c7d2aa329b7f
awesomegusS <godswillikwan01@gmail.com>
2021-11-04 11:17:32 +0100

    create window, label, add entry, add button

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
 import tkinter as tk
+import sys
 
 # a function
-def statement(event):
-    print("I still can't see my label")
+# def statement(event):
+#     print(sys.path)
 
 # creating a window
-
 window = tk.Tk()
 window.title('spotify_automator')
 window.geometry('300x500')
 
+
 # place a text in the app
-label_name = tk.Label(window, text='some_text')
+label_name = tk.Label(window, text='enter your name:')
 label_name.grid(row=0, column=1)
 # label_name.pack(side=tk.TOP) # bug
 
+
+# add an entry
+name_entry = tk.Entry(window)
+name_entry.grid(row=1, column=0)
+
+def get_input():
+    name = name_entry.get()
+    print('username is {}'.format(name))
+
+
 # add a button
-button_name = tk.Button(window, text='activate')
-button_name.grid(row=150, column=150)
-button_name.bind('<Button-1>', statement)
+button_name = tk.Button(window, text='submit', command=get_input)
+button_name.grid(row=3, column=1)
 
 window.mainloop()
